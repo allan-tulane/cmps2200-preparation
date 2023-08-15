e7a00e5d83656a3768940a4fb3f4ebf0322aecff
Zhengming Ding <allanzmding@gmail.com>
2023-08-15 19:53:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ def myfunction(input):
 	""" done. """
 	return input
 
-def test_myfunction():
-	""" done. """
-	assert myfunction('Hello') == 'Hello'
-	assert myfunction(1) == 1
-	assert myfunction(6) == -1
+
 
 
