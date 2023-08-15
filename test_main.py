fd27c597c576cfc614d90301746c3a28d8d276a9
Zhengming Ding <allanzmding@gmail.com>
2023-08-15 19:53:45 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,23 +1,7 @@
 from main import *
 
-def test_compare_search():
-	res = compare_search(sizes=[10, 100])
-	print(res)
-	assert res[0][0] == 10
-	assert res[1][0] == 100
-	assert res[0][1] < 1
-	assert res[1][1] < 1
-
-def test_binary_search():
-	assert binary_search([1,2,3,4,5], 5) == 4
-	assert binary_search([1,2,3,4,5], 1) == 0
-	assert binary_search([1,2,3,4,5], 6) == -1
-	### TODO: add two more tests here.
-
-	###
-
-def test_linear_search():
+def test_myfunction():
 	""" done. """
-	assert linear_search([1,2,3,4,5], 5) == 4
-	assert linear_search([1,2,3,4,5], 1) == 0
-	assert linear_search([1,2,3,4,5], 6) == -1
+	assert myfunction('Hello') == 'Hello'
+	assert myfunction(1) == 1
+	assert myfunction(6) == -1
